e509d6ef787d71014c820372e561f5035fd50ce7
loloolk <marc.bondoger@gmail.com>
2024-11-18 11:09:59 -0800

    Added support for any size

--- MyVec.cpp
+++ MyVec.cpp
@@ -7,31 +7,43 @@
 
 template<typename T> class MyVec {
     public:
-    T* data[10];
-    int len;
+    int len; // current amount of items
     int capacity; // max amount of items
     int sub_len; // max amount of items in each sub array
+    T** data;
 
     MyVec() {
         len = 0;
-        capacity = 10;
+
+        capacity = 100;
         sub_len = 10;
+
+        data = (T**)malloc(sizeof(T*) * ceil((float)capacity / sub_len));
+        data[0] = (T*)malloc(sizeof(T) * sub_len);
+    }
+    MyVec(int cap) {
+        len = 0;
+
+        capacity = cap;
+        sub_len = 10;
+
+        data = (T**)malloc(sizeof(T*) * ceil((float)capacity / sub_len));
         data[0] = (T*)malloc(sizeof(T) * sub_len);
     }
     ~MyVec() {
         for (int i = 0; i < ceil((float)len / 10); i++) {
             free(data[i]);
         }
+        free(data);
     }
 
     void push(T val) {
         if (len == capacity) {
-            if (capacity == 100) {
-                std::cout << "Vector is full" << std::endl;
-                return;
-            }
-            data[(int)ceil((float)len / 10)] = (T*) malloc(sizeof(T) * 10);
-            capacity += 10;
+            std::cout << "Vector is full" << std::endl;
+            return;
+        }
+        else if (len % sub_len == 0 && len != 0) {
+            data[(int)floor((float)len / 10)] = (T*)malloc(sizeof(T) * sub_len);
         }
 
         data[(int)floor((float)len / 10)][len % 10] = val;
@@ -39,7 +51,7 @@ template<typename T> class MyVec {
     }
 
     T get(int index) {
-        if (index >= len) {
+        if (index < 0 || index >= len) {
             std::cout << "Index out of bounds" << std::endl;
             exit(1);
         }
